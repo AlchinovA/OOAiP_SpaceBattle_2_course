0512be9505a86d39cdd0a89a1e506d8ceba36eca
Polina <polina.andreevna.4709@mail.ru>
2023-01-12 20:20:00 +0600

    add tests

--- SpaceBattle.Lib.Test/StartMoveCommandTest.cs
+++ SpaceBattle.Lib.Test/StartMoveCommandTest.cs
@@ -5,7 +5,7 @@ namespace SpaceBattle.Lib.Test;
 public class StartMoveCommandTest
 {
     [Fact]
-    public void MoveCommandContinious()
+    public void MoveCommandStartableTest()
     {
         new Hwdtech.Ioc.InitScopeBasedIoCImplementationCommand().Execute();
 
@@ -15,10 +15,10 @@ public class StartMoveCommandTest
             MovableAdapter adapter = new MovableAdapter(args);
             return adapter;
         }).Execute();
-
+        
         Mock<IUObject> command = new();
         Mock<IUObject> obj = new();
-        Queue<SpaceBattle.Lib.ICommand> queue = new();
+        Queue<ICommand> queue = new();
 
         obj.Setup(x => x.get_property("velocity")).Returns((object) new Vector(1, 1));
         obj.Setup(x => x.get_property("position")).Returns((object) new Vector(0, 0));
@@ -30,5 +30,33 @@ public class StartMoveCommandTest
         StartMoveCommand start = new(command.Object);
         start.Execute();
         Assert.Single(queue);
+        queue.Dequeue().Execute();
+    }
+    [Fact]
+    public void AddCommandTest()
+    {
+        Mock<SpaceBattle.Lib.ICommand> command = new();
+        Queue<SpaceBattle.Lib.ICommand> queue = new();
+        new AddCommand(command.Object, queue).Execute();
+    }
+    [Fact]
+    public void MovableAdapterTest()
+    {
+        new Hwdtech.Ioc.InitScopeBasedIoCImplementationCommand().Execute();
+
+        Hwdtech.IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", Hwdtech.IoC.Resolve<object>("Scopes.New", Hwdtech.IoC.Resolve<object>("Scopes.Root"))).Execute();
+
+        Hwdtech.IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Adapters.IUObject.Movable", (object[] args) => 
+        {
+            MovableAdapter adapter = new MovableAdapter(args);
+            return adapter;
+        }).Execute();
+
+        Mock<IUObject> obj = new();
+        obj.Setup(x => x.get_property("position")).Returns(new Vector(8, 8));
+        obj.Setup(x => x.get_property("velocity")).Returns(new Vector(7, 7));
+        IMovable movable = Hwdtech.IoC.Resolve<IMovable>("Adapters.IUObject.Movable", obj.Object);
+
+        Assert.Equal(movable.velocity, new Vector(7, 7));
     }
 }
